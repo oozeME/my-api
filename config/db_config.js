e1b7e27fbb4243fa786bd14f0454aa30d353d233
dasheng <1666795332@qq.com>
2017-11-01 17:34:53 +0800

    my-api

---
+++ config/db_config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // dev_db: {
+    //     host: "localhost",
+    //     user: "root",
+    //     password: "5959",
+    //     port: 3306,
+    //     database: "blog_sql"
+    // },
+    dev_db: {
+        host: "47.94.148.114",
+        user: "root",
+        password: "5959",
+        port: 3306,
+        database: "blog_sql"
+    }
+}
